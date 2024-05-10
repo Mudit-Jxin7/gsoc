05b06f02d97b964d08f800ff74fc27f555991c15
Mudit-Jxin7 <muditert34@gmail.com>
2024-05-10 14:55:18 +0530

    feat: pagination

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,38 @@ import Card from "@/components/Card";
 
 export default function Home() {
   const [organizations, setOrganizations] = useState(allYears);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const organizationsToShow = organizations.slice(
+    (currentPage - 1) * 12,
+    currentPage * 12
+  );
+
   return (
-    <div className="bg-[#d3cfcf]">
-      {/* {organizations.map((organization, idx) => {
-        return (
-          <div className="text-black" key={idx}>
-            <h1>{organization.name}</h1>
-          </div>
-        );
-      })} */}
-      <Card />
+    <div className="bg-[#fdfbfb]">
+      <div className="grid grid-cols-4 gap-8 px-10">
+        {organizationsToShow.map((organization, idx) => {
+          return (
+            <div key={idx}>
+              <Card organization={organization} />{" "}
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex text-black mt-8 gap-6 justify-center">
+        <button
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={currentPage === Math.ceil(organizations.length / 12)}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -2,30 +2,39 @@ import React from "react";
 import Image from "next/image";
 import { ArrowUpRight } from "@phosphor-icons/react";
 
-const Card = () => {
+const Card = ({ organization }: any) => {
   return (
-    <div className="hover:scale-105 transition-all flex text-black bg-[#d3cfcf] shadow-2xl flex-col gap-6 w-1/5 h-96 rounded-2xl justify-center items-center">
+    <div className="hover:scale-105 transition-all flex text-black bg-[#fdfbfb] shadow-2xl flex-col gap-6 w-full h-96 rounded-2xl justify-center items-center">
       <Image
-        src="https://lh3.googleusercontent.com/5bb1zuSJfNaskJVgD5jO4Mdzamk1udiUH1BXVSzMcM8fB-kIy62kBFS6MtQj1EW445ZkQ1BWlvJL2oXRJNqsYBcmPWpSXng"
+        src={organization.image_url}
         alt={"img"}
         width={100}
         height={100}
       />
-      <div className="font-semibold text-xl items-center flex flex-row gap-4">
-        <p>3DTK</p>
+      <div className="font-semibold px-6 mx-auto text-lg items-center flex flex-row gap-2">
+        <p>{organization.name}</p>
         <ArrowUpRight size={32} />
       </div>
-      <p className="px-10 mx-auto">
-        The 3D Toolkit provides algorithms and methods to process 3D point
-        clouds
-      </p>
+      <p className="px-10 mx-auto">{organization.description}</p>
       <ul className="flex flex-wrap gap-5 px-10">
-        <li className="bg-[#141417] text-white inline-block text-center p-2 rounded-md mx-auto">c</li>
-        <li className="bg-[#141417] text-white inline-block text-center p-2 rounded-md mx-auto">c++</li>
-        <li className="bg-[#141417] text-white inline-block text-center p-2 rounded-md mx-auto">ros</li>
-        <li className="bg-[#141417] text-white inline-block text-center p-2 rounded-md mx-auto">opencv</li>
-        <li className="bg-[#141417] text-white inline-block text-center p-2 rounded-md mx-auto">cmake</li>
-        <li className="bg-[#141417] text-white inline-block text-center p-2 rounded-md mx-auto">boost</li>
+        <li className="bg-[#141417] text-white inline-block text-center p-2 rounded-md mx-auto">
+          c
+        </li>
+        <li className="bg-[#141417] text-white inline-block text-center p-2 rounded-md mx-auto">
+          c++
+        </li>
+        <li className="bg-[#141417] text-white inline-block text-center p-2 rounded-md mx-auto">
+          ros
+        </li>
+        <li className="bg-[#141417] text-white inline-block text-center p-2 rounded-md mx-auto">
+          opencv
+        </li>
+        <li className="bg-[#141417] text-white inline-block text-center p-2 rounded-md mx-auto">
+          cmake
+        </li>
+        <li className="bg-[#141417] text-white inline-block text-center p-2 rounded-md mx-auto">
+          boost
+        </li>
       </ul>
     </div>
   );
